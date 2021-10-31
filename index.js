2184a2a74f8ba2ca071eb8b9e8f29a6564adfbe6
ireprised <ireprised@gmail.com>
2021-10-31 14:20:20 +0600

    db data updated

--- index.js
+++ index.js
@@ -11,12 +11,29 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.texip.mongodb.net/volunteerNetwork?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3pim7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+async function run(){
+  try{
+    await client.connect();
+    const database = client.db("ServiceCluster")
+    const serviceCollection = database.collection("services");
+    
+    //get api
+    app.get('/services',async(req,res)=>{
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services)
+    })
+  }
+  finally{
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send('db connected');
